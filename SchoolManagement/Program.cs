55ccf16971d46e15bfb230ae75d0774264b0c704
My <my.j97@protonmail.com>
2024-02-10 22:26:00 +0100

    connection to database done

--- SchoolManagement/Program.cs
+++ SchoolManagement/Program.cs
@@ -1,6 +1,13 @@
-﻿using Microsoft.Extensions.Hosting;
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using SchoolManagement.Contexts;
 
 var builder = Host.CreateDefaultBuilder().ConfigureServices(services =>
 {
+
+
+    services.AddDbContext<DataContexts>(x => x.UseSqlServer(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Projects\SQL\SchoolManagement\SchoolManagement\Data\database_school.mdf;Integrated Security=True;Connect Timeout=30"));
+
 
 }).Build();
