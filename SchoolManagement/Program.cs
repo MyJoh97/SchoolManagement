1319ef39ffeb708ce5eefc9956c9cbd88642d714
My <my.j97@protonmail.com>
2024-02-11 22:44:24 +0100

    UI started

--- SchoolManagement/Program.cs
+++ SchoolManagement/Program.cs
@@ -2,12 +2,27 @@
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using SchoolManagement.Contexts;
+using SchoolManagement.Repositories;
+using SchoolManagement.Services;
 
 var builder = Host.CreateDefaultBuilder().ConfigureServices(services =>
 {
 
 
     services.AddDbContext<DataContexts>(x => x.UseSqlServer(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Projects\SQL\SchoolManagement\SchoolManagement\Data\database_school.mdf;Integrated Security=True;Connect Timeout=30"));
+
+    services.AddScoped<ContactInfoRepository>();
+    services.AddScoped<SchoolRepository>();
+    services.AddScoped<RoleRepository>();
+    services.AddScoped<CourseRepository>();
+    services.AddScoped<MemberRepository>();
+
+
+    services.AddScoped<ContactInfoService>();
+    services.AddScoped<SchoolService>();
+    services.AddScoped<RoleService>();
+    services.AddScoped<CourseService>();
+    services.AddScoped<MemberService>();
 
 
 }).Build();
---
+++ SchoolManagement/SchoolManagementUI.cs
@@ -0,0 +1,43 @@
+﻿using SchoolManagement.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolManagement
+{
+    internal class SchoolManagementUI
+    {
+
+        private readonly CourseService _courseService;
+
+        public SchoolManagementUI(CourseService courseService)
+        {
+            _courseService = courseService;
+        }
+
+
+        public void CreateCourse_UI()
+        {
+            Console.Clear();
+            Console.WriteLine("### Create a Course ###");
+
+            Console.Write("Course Name: ");
+            var courseTitle = Console.ReadLine()!;
+
+            Console.Write("School Name: ");
+            var schoolName = Console.ReadLine()!;
+
+            var result = _courseService.CreateCourse(courseTitle, schoolName);
+            if (result != null)
+            {
+                Console.Clear();
+                Console.WriteLine("The Course Was Created!");
+            }
+
+
+        }
+
+    }
+}
