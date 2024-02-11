9c247292f323a74e0561bec04a9fab7fcc845b66
My <my.j97@protonmail.com>
2024-02-11 13:37:13 +0100

    member service started

---
+++ SchoolManagement/Services/MemberService.cs
@@ -0,0 +1,23 @@
+﻿using SchoolManagement.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolManagement.Services
+{
+    internal class MemberService
+    {
+        private readonly MemberRepository _memberRepository;
+        private readonly ContactInfoService _contactInfoService;
+        private readonly RoleService _roleService;
+
+        public MemberService(MemberRepository memberRepository, ContactInfoService contactInfoService, RoleService roleService)
+        {
+            _memberRepository = memberRepository;
+            _contactInfoService = contactInfoService;
+            _roleService = roleService;
+        }
+    }
+}
